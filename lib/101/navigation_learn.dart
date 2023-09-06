ada2b6403c3a2c1a9ad805b6c018f035156e1287
onalanrecep <onalanrecep86@gmail.com>
2023-09-06 22:40:48 +0300

    navigateLearn and Detail Completed

--- lib/101/navigation_learn.dart
+++ lib/101/navigation_learn.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:zero_to_hero_flutter/101/icon_learn.dart';
+//import 'package:zero_to_hero_flutter/101/icon_learn.dart';
+import 'package:zero_to_hero_flutter/101/navigate_detail_learn.dart';
 
 class NavigationLearn extends StatefulWidget {
   const NavigationLearn({super.key});
@@ -10,6 +11,13 @@ class NavigationLearn extends StatefulWidget {
 
 class _NavigationLearnState extends State<NavigationLearn>
     with NavigatorManager {
+  List<int> selecetedItems = [];
+  void addSelected(int index, bool isAdd) {
+    setState(() {
+      isAdd ? selecetedItems.add(index) : selecetedItems.remove(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +25,25 @@ class _NavigationLearnState extends State<NavigationLearn>
         title: const Text('Navigation Learn'),
       ),
       body: ListView.builder(itemBuilder: (context, index) {
-        return const Placeholder(
-          color: Colors.red,
+        return TextButton(
+          onPressed: () async {
+            final response = await navigateToWidgetNormal<bool>(context,
+                NavigateDetailLearn(isOkey: selecetedItems.contains(index)));
+            if (response is bool) {
+              addSelected(index, response);
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Placeholder(
+              color: selecetedItems.contains(index) ? Colors.green : Colors.red,
+            ),
+          ),
         );
       }),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.navigation_rounded),
-        onPressed: () {
-          navigateToWidget(context, IconLearnView());
-        },
+        onPressed: () {},
       ),
     );
   }
@@ -34,6 +52,18 @@ class _NavigationLearnState extends State<NavigationLearn>
 mixin NavigatorManager {
   void navigateToWidget(BuildContext context, Widget widget) {
     Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return widget;
+        },
+        fullscreenDialog: true,
+        settings: const RouteSettings(),
+      ),
+    );
+  }
+
+  Future<T?> navigateToWidgetNormal<T>(BuildContext context, Widget widget) {
+    return Navigator.of(context).push<T>(
       MaterialPageRoute(
         builder: (context) {
           return widget;
