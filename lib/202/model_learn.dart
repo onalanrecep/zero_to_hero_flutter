169598399ae872fad7f2419d4799687ea02e9696
onalanrecep <onalanrecep86@gmail.com>
2023-09-11 13:47:37 +0300

    Model Learn Completed

---
+++ lib/202/model_learn.dart
@@ -0,0 +1,55 @@
+class PostModel {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+}
+
+class PostModel2 {
+  int userId;
+  int id;
+  String title;
+  String body;
+
+  PostModel2(this.userId, this.id, this.title, this.body);
+}
+
+class PostModel3 {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  PostModel3(this.userId, this.id, this.title, this.body);
+}
+
+class PostModel4 {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  PostModel4(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.body});
+}
+
+class PostModel5 {
+  final int _userId;
+  final int _id;
+  final String _title;
+  final String _body;
+
+  int get userId => _userId;
+  PostModel5({
+    required int userId,
+    required int id,
+    required String title,
+    required String body,
+  })  : _userId = userId,
+        _id = id,
+        _title = title,
+        _body = body;
+}
